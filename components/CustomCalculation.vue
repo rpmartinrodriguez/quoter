b234b0e6a77ce2476bdaff4d744428e1d747b3db
rpmartinrodriguez <rp.rodriguezmartin@gmail.com>
2025-05-30 12:57:16 -0300

    Update CustomCalculation.vue

--- components/CustomCalculation.vue
+++ components/CustomCalculation.vue
@@ -5,7 +5,7 @@
     <div>
       <b>Precio Total:</b>
       <div class="mt-2">
-        {{ formatAsArs(total) }}
+        {{ formatAsArs(props.total || 0) }}
       </div>
     </div>
 
@@ -23,7 +23,7 @@
       ></v-text-field>
     </div>
 
-    <!-- Custom Deposito -->
+    <!-- Custom Depósito -->
     <div>
       <b>Depósito Personalizado:</b>
       <br />
@@ -49,13 +49,14 @@
       </div>
     </div>
   </div>
-  <br />
-  <br />
+
+  <br /><br />
+
   <!-- Calculated Quotes -->
   <div v-if="showQuotes" class="custom-calculations-wrapper">
     <div class="text-center">
       <p><strong>Monto a financiar</strong></p>
-      <p class="mt-2">{{ formatAsArs(toFinance) }}</p>
+      <p class="mt-2">{{ formatAsArs(toFinance || 0) }}</p>
     </div>
 
     <div v-for="cq in calculatedQuotes" :key="cq.$id" class="text-center">
@@ -76,84 +77,61 @@
               <v-icon>mdi-content-copy</v-icon>
             </v-btn>
           </template>
-          Copiar 3 cuotas (39.07%)
+          Copiar {{ cq.quantity }} cuotas ({{ cq.percentage }})
         </v-tooltip>
       </p>
     </div>
   </div>
+
   <div v-else class="text-center">
     Indique un depósito personalizado para calcular las cuotas
   </div>
 </template>
+
 <script lang="ts" setup>
 import { useClipboard } from "@vueuse/core";
 
-const { products, total } = defineProps<{
+const props = defineProps<{
   products: string[];
   total: number;
 }>();
 
 const { formatAsArs } = useFormatters();
-
-const calculatedQuotes = ref<any>([]);
-const showQuotes = computed<boolean>(() => calculatedQuotes.value.length > 0);
-const toFinance = ref<number>(0);
-
 const { quotes } = useQuote();
-const customQuotes = ref([]);
+
+const calculatedQuotes = ref<any[]>([]);
+const customQuotes = ref<any[]>([]);
+const showQuotes = computed(() => calculatedQuotes.value.length > 0);
+const toFinance = ref<number>(0);
 const customTotal = ref<number>();
 const customDeposit = ref<number>();
+
 const disableCalculate = computed(() => {
   if (!customDeposit.value) return true;
-  else if (
-    customDeposit.value &&
-    !customTotal.value &&
-    customDeposit.value > total
-  )
-    return true;
-  else if (
-    customDeposit.value &&
-    customTotal.value &&
-    customDeposit.value > customTotal.value
-  )
-    return true;
-  else return false;
+  if (customTotal.value && customDeposit.value > customTotal.value) return true;
+  if (!customTotal.value && customDeposit.value > props.total) return true;
+  return false;
 });
-const setCustomTotal = (event: string) => {
-  if (
-    isNaN(event as unknown as number) &&
-    !isFinite(event as unknown as number)
-  )
-    return;
-  if (event.length === 0) {
-    customTotal.value = undefined;
-    return;
-  }
-  customTotal.value = event as unknown as number;
-};
-const setCustomDeposit = (event: string) => {
-  if (
-    isNaN(event as unknown as number) &&
-    !isFinite(event as unknown as number)
-  )
-    return;
-  if (event.length === 0) {
-    customDeposit.value = undefined;
-    return;
-  }
-  customDeposit.value = event as unknown as number;
-};
-const calculate = () => {
-  const qcs = quotes.value.map((q) => {
-    let amount = 0;
-    toFinance.value = 0;
-    if (!customTotal.value) {
-      toFinance.value = total - customDeposit.value!;
-    } else {
-      toFinance.value = customTotal.value - customDeposit.value!;
-    }
-    amount = (toFinance.value * q.percentage) / 100;
 
+const setCustomTotal = (value: string) => {
+  const num = parseFloat(value);
+  customTotal.value = isNaN(num) ? undefined : num;
+};
+
+const setCustomDeposit = (value: string) => {
+  const num = parseFloat(value);
+  customDeposit.value = isNaN(num) ? undefined : num;
+};
+
+const calculate = () => {
+  if (!customDeposit.value) return;
+
+  toFinance.value = customTotal.value
+    ? customTotal.value - customDeposit.value
+    : props.total - customDeposit.value;
+
+  calculatedQuotes.value = quotes.value.map((q) => {
+    const amount = (toFinance.value * q.percentage) / 100;
     return {
       $id: q.$id,
       quantity: q.quantity,
@@ -161,33 +139,35 @@ const calculate = () => {
       amount: formatAsArs(amount),
     };
   });
-  calculatedQuotes.value = qcs;
 };
 
 const source = ref("");
 const { copy } = useClipboard({ source });
-const handleCopyClick = (quotes: any) => {
+
+const handleCopyClick = (quote: any) => {
+  const depositStr = formatAsArs(customDeposit.value || 0);
+  const quoteAmount = quote.amount;
+
   source.value = `Hola!!
 Quería agradecerte por la excelente decisión que tomaste. Te hacemos un breve resumen para que tengas toda la información a mano:
 
-\t•\t*Pieza${
-    products.length > 1 ? `s: ${products.join(", ")}` : `: ${products[0]}`
-  }*
-\t•\tDepósito inicial: ${formatAsArs(customDeposit.value!)}
-\t•\tCantidad de cuotas: ${quotes.quantity}
-\t•\tValor de cada cuota: ${quotes.amount}
+\t•\t*Pieza${props.products.length > 1 ? `s: ${props.products.join(", ")}` : `: ${props.products[0]}`}*
+\t•\tDepósito inicial: ${depositStr}
+\t•\tCantidad de cuotas: ${quote.quantity}
+\t•\tValor de cada cuota: ${quoteAmount}
 
 Estamos seguros de que esta decisión cumplirá con todas tus expectativas. Cualquier consulta o duda que tengas, no dudes en contactarnos. ¡Gracias por confiar en nosotros! Royal Prestige!
 
 A continuación, unos links de interés:
 
-\t•\Curado de Ollas: https://www.youtube.com/watch?app=desktop&v=m0SAopwbgxc
-\t•\Recetas: https://www.royalprestige.com/ar/inspiracion/recetas
+\t•\tCurado de Ollas: https://www.youtube.com/watch?v=m0SAopwbgxc
+\t•\tRecetas: https://www.royalprestige.com/ar/inspiracion/recetas
+\t•\tInstagram: https://www.instagram.com/royalprestigeargoficial`;
 
-Seguinos en Instagram ➡️ https://www.instagram.com/royalprestigeargoficial?igsh=MXI0ZDN4ZWR1OXlyaA==`;
   copy(source.value);
 };
 </script>
+
 <style>
 .setup-wrapper {
   column-gap: 3em;
