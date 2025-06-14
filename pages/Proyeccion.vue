2d1749da83f8679bd065751ce82b52bf8b93842e
rpmartinrodriguez <rp.rodriguezmartin@gmail.com>
2025-06-14 19:12:07 -0300

    Create Proyeccion.vue

---
+++ pages/Proyeccion.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-card flat>
+    <v-card-title class="d-flex align-center mb-4">
+      <v-icon start>mdi-finance</v-icon>
+      Proyección de Ventas
+    </v-card-title>
+    <v-card-text>
+      <p class="text-center text-grey">
+        Próximamente: Herramienta de simulación y proyección de objetivos.
+      </p>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script lang="ts" setup>
+// La magia vendrá aquí en el siguiente paso
+</script>
