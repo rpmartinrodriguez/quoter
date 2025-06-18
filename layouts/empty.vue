8fd67cb78bb53793adb550a49158e47ba3363c9c
rpmartinrodriguez <rp.rodriguezmartin@gmail.com>
2025-06-18 20:48:03 -0300

    Update empty.vue

--- layouts/empty.vue
+++ layouts/empty.vue
@@ -1,4 +1,3 @@
-// layouts/empty.vue
 <template>
   <v-app>
     <v-main style="background-color: #f5f5f5;">
@@ -6,7 +5,3 @@
     </v-main>
   </v-app>
 </template>
-
-<script lang="ts" setup>
-// No necesita lógica
-</script>
