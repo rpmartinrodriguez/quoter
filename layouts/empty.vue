403570d0013fb1c66c122fc8da8b899426e6d2ec
rpmartinrodriguez <rp.rodriguezmartin@gmail.com>
2025-06-18 20:32:14 -0300

    Create empty.vue

---
+++ layouts/empty.vue
@@ -0,0 +1,12 @@
+// layouts/empty.vue
+<template>
+  <v-app>
+    <v-main style="background-color: #f5f5f5;">
+      <slot />
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+// No necesita lógica
+</script>
