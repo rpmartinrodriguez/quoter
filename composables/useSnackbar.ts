d55fbb2f892587ee9044f206bb6e711f69f649f7
rpmartinrodriguez <rp.rodriguezmartin@gmail.com>
2025-06-18 15:32:55 -0300

    Update useSnackbar.ts

--- composables/useSnackbar.ts
+++ composables/useSnackbar.ts
@@ -1,14 +1,16 @@
 // composables/useSnackbar.ts
 import { ref } from 'vue';
 
+// Estado global y compartido para la notificación
 const show = ref(false);
 const text = ref('');
-const color = ref('success');
+const color = ref('success'); // Color por defecto
 
 export const useSnackbar = () => {
+  // Función para mostrar la notificación desde cualquier parte de la app
   const showSnackbar = (options: { text: string; color?: string }) => {
     text.value = options.text;
-    color.value = options.color || 'success';
+    color.value = options.color || 'success'; // Si no se especifica color, será verde
     show.value = true;
   };
 
