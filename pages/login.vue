7094c9c38c6d4810e0220cc8212b271e2b5c9bb8
rpmartinrodriguez <rp.rodriguezmartin@gmail.com>
2025-06-18 21:42:24 -0300

    Update login.vue

--- pages/login.vue
+++ pages/login.vue
@@ -6,33 +6,38 @@
           <v-toolbar color="primary" dark flat>
             <v-toolbar-title>Iniciar Sesión</v-toolbar-title>
           </v-toolbar>
-          <v-card-text>
+          <v-card-text class="pt-4">
             <v-form @submit.prevent="handleLogin">
               <v-text-field
                 v-model="email"
                 label="Email"
                 name="login"
-                prepend-icon="mdi-account"
+                prepend-inner-icon="mdi-account"
                 type="text"
-                variant="underlined"
-                class="mt-4"
+                variant="outlined"
+                density="compact"
+                class="mb-2"
               ></v-text-field>
+
               <v-text-field
                 v-model="password"
                 label="Contraseña"
                 name="password"
-                prepend-icon="mdi-lock"
-                type="password"
-                variant="underlined"
+                prepend-inner-icon="mdi-lock"
+                variant="outlined"
+                density="compact"
+                :type="showPassword ? 'text' : 'password'"
+                :append-inner-icon="showPassword ? 'mdi-eye-off' : 'mdi-eye'"
+                @click:append-inner="showPassword = !showPassword"
               ></v-text-field>
               <v-alert v-if="errorMsg" type="error" density="compact" class="mt-4">
                 {{ errorMsg }}
               </v-alert>
             </v-form>
           </v-card-text>
-          <v-card-actions>
+          <v-card-actions class="px-4 pb-4">
             <v-spacer></v-spacer>
-            <v-btn color="primary" @click="handleLogin" :loading="isLoading" size="large">Entrar</v-btn>
+            <v-btn color="primary" @click="handleLogin" :loading="isLoading" size="large" block>Entrar</v-btn>
           </v-card-actions>
         </v-card>
       </v-col>
@@ -41,16 +46,21 @@
 </template>
 
 <script lang="ts" setup>
+// Le decimos a Nuxt que esta página use el layout "empty".
 definePageMeta({
   layout: 'empty',
 });
 
 const { login } = useAuth();
 const router = useRouter();
+
 const email = ref('');
 const password = ref('');
 const errorMsg = ref('');
 const isLoading = ref(false);
+
+// ✅ Se añade una variable para controlar la visibilidad de la contraseña
+const showPassword = ref(false);
 
 const handleLogin = async () => {
   if (!email.value || !password.value) {
