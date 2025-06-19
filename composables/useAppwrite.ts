1e9affb484ed154fe472ee59e4afe4e3c3c8669d
rpmartinrodriguez <rp.rodriguezmartin@gmail.com>
2025-06-19 17:02:15 +0000

    Update useAppwrite.ts

--- composables/useAppwrite.ts
+++ composables/useAppwrite.ts
@@ -1,9 +1,7 @@
-import { Client, Databases, Account } from "appwrite";
+import { Client, Databases } from "appwrite";
 
-// Instancias únicas para toda la aplicación
 let client: Client | null = null;
 let databases: Databases | null = null;
-let account: Account | null = null;
 
 export const useAppwrite = () => {
   const config = useRuntimeConfig();
@@ -13,18 +11,10 @@ export const useAppwrite = () => {
       .setEndpoint(config.public.endpoint)
       .setProject(config.public.project);
   }
-
   if (!databases) {
     databases = new Databases(client);
   }
-
-  if (!account) {
-    account = new Account(client);
-  }
-
-  // ✅ Es crucial que retorne tanto 'databases' como 'account'
   return {
     databases,
-    account,
   };
 };
