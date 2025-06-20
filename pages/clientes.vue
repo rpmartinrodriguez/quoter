c63e13d47cfeb0e506f84547bbca55c43b7e99e8
rpmartinrodriguez <rp.rodriguezmartin@gmail.com>
2025-06-20 17:02:41 -0300

    Create clientes.vue

---
+++ pages/clientes.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-card flat>
+    <v-card-title class="d-flex align-center">
+      <v-icon start>mdi-account-box-multiple-outline</v-icon>
+      Directorio de Clientes
+    </v-card-title>
+    <v-card-text>
+      <p class="text-center text-grey pa-4">
+        Próximamente: Tabla con la lista de todos tus clientes.
+      </p>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script lang="ts" setup>
+import { onMounted } from 'vue';
+import { usePageTitle } from '~/composables/usePageTitle';
+
+const { setTitle } = usePageTitle();
+
+onMounted(() => {
+  setTitle('Clientes');
+});
+</script>
