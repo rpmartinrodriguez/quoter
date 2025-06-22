41ed7dcd3ccebb4cc74cfe27da54048b241549d0
rpmartinrodriguez <rp.rodriguezmartin@gmail.com>
2025-06-22 16:41:22 -0300

    Update auth.global.ts

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,16 +1,10 @@
 // middleware/auth.global.ts
 export default defineNuxtRouteMiddleware((to, from) => {
   const { user } = useAuth();
-
-  // Si NO hay usuario Y la página a la que se quiere ir NO es /login...
   if (!user.value && to.path !== '/login') {
-    // ...lo mandamos a la página de login.
     return navigateTo('/login');
   }
-
-  // Si SÍ hay usuario Y la página a la que quiere ir ES /login...
   if (user.value && to.path === '/login') {
-    // ...lo mandamos a la página principal.
     return navigateTo('/');
   }
 });
