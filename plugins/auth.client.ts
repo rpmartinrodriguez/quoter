be2fa1ce7beb82ee08b60beb6606e4c64c1423b0
rpmartinrodriguez <rp.rodriguezmartin@gmail.com>
2025-06-22 16:38:31 -0300

    Update auth.client.ts

--- plugins/auth.client.ts
+++ plugins/auth.client.ts
@@ -1,9 +1,7 @@
 // plugins/auth.client.ts
 export default defineNuxtPlugin(async (nuxtApp) => {
   const { fetchCurrentUser } = useAuth();
-
-  // Se ejecuta una sola vez en el lado del cliente cuando la app se inicializa.
-  // El 'await' asegura que esperamos a tener una respuesta de Appwrite
-  // antes de que el resto de la app (como el middleware) continúe.
+  // Esto se ejecuta al arrancar la app y verifica si ya hay una sesión
+  // antes de que cualquier otra cosa intente acceder a las páginas.
   await fetchCurrentUser();
 });
